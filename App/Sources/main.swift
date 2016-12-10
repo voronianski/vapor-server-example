58de7c04a282b10d28817fa3022fb3ebc963cb6d
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2016-12-10 02:14:50 +0100

    add json

--- App/Sources/main.swift
+++ App/Sources/main.swift
@@ -10,4 +10,16 @@ drop.get("hash", String.self) { req, name in
   return try drop.hash.make(name)
 }
 
+drop.get("json") { req in
+  return try JSON(node: [
+    "foo": "bar",
+    "num": 123,
+    "array": [1, 2, 3],
+    "dict": [
+      "name": "Vapor",
+      "lang": "Swifty"
+    ]
+  ])
+}
+
 drop.run()
